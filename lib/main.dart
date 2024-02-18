40e544dd98ebad03de39121237d413adb3fc13db
Lucas <Lucas>
2024-02-18 14:31:37 +0100

    erster test

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,283 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+// Definiere die Karte
+class Card {
+  String suit;
+  int rank; // Rank ist nun ein Integer
+  String imagePath;
+
+  Card({required this.suit, required this.rank, required this.imagePath});
+
+  @override
+  String toString() {
+    return 'Suit: $suit, Rank: $rank, ImagePath: $imagePath';
+  }
+
+  String get uniqueKey => '$suit$rank';
+}
+
+
+// Erstelle ein Kartendeck
+List<Card> createDeck() {
+  List<String> suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+  List<int> ranks = List.generate(13, (index) => index + 2); // Generiere Ränge von 2 bis 14 (Ace)
+  List<Card> deck = [];
+
+  for (var suit in suits) {
+    for (var rank in ranks) {
+      String imagePath = '/${suit}${rank}';
+      deck.add(Card(suit: suit, rank: rank, imagePath: imagePath));
+    }
+  }
+
+  return deck;
+}
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Titel, Theme und Dark Theme definieren
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        // Definiere ein helles Theme
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        // Definiere ein dunkles Theme
+        brightness: Brightness.dark,
+      ),
+      // Automatischer Wechsel zwischen hellem und dunklem Theme
+      themeMode: ThemeMode.system, // Benutzt Systemeinstellungen für den Theme-Modus
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class RulesPage extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Spielregeln'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(
+          'Hier stehen die Spielregeln... \n\n'
+          '1. Regel 1 \n\n'
+          '2. Regel 2 \n\n'
+          '3. Regel 3 \n\n'
+          'Füge hier weitere Regeln hinzu.',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
+class _HomePageState extends State<HomePage> {
+  int spielerzahl = 4; // Startwert
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Startseite'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.remove),
+                  onPressed: () {
+                    setState(() {
+                      if (spielerzahl > 1) spielerzahl--;
+                    });
+                  },
+                ),
+                Text(
+                  spielerzahl.toString(),
+                  style: TextStyle(fontSize: 24.0),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    setState(() {
+                      if (spielerzahl < 10) spielerzahl++;
+                    });
+                  },
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 10), // Fügt Platz unter dem Spieler-Counter hinzu
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50), // Steuert die Breite der Buttons
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GamePage(spielerzahl: spielerzahl)),
+                  );
+                },
+                child: Text('Spielen'),
+              ),
+            ),
+            SizedBox(height: 10), // Abstand zwischen den Buttons
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100), // Steuert die Breite der Buttons
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RulesPage()),
+                  );
+                },
+                child: Text('Spielregeln'),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  final int spielerzahl;
+
+  GamePage({required this.spielerzahl});
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  List<Card> deck = [];
+  List<Card> hand = [];
+  Random random = Random();
+  List<bool?> playerGuesses = [];
+  Card? selectedCard;
+
+  @override
+  void initState() {
+    super.initState();
+    deck = createDeck();
+    drawCards();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      askPlayerGuess();
+    });
+  }
+
+  void drawCards() {
+    Set<String> drawnCards = Set();
+    while (hand.length < 4) {
+      Card card = deck[random.nextInt(deck.length)];
+      if (!drawnCards.contains(card.uniqueKey)) {
+        drawnCards.add(card.uniqueKey);
+        hand.add(card);
+      }
+    }
+    selectedCard = hand[random.nextInt(hand.length)]; // Wähle zufällig eine Karte aus den gezogenen Karten
+  }
+
+  void askPlayerGuess() {
+    if (playerGuesses.length < widget.spielerzahl) {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Spieler ${playerGuesses.length + 1}, ist die Karte rot oder schwarz?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  playerGuesses.add(true); // Rot
+                  Navigator.of(context).pop();
+                  askPlayerGuess();
+                },
+                child: Text('Rot'),
+              ),
+              TextButton(
+                onPressed: () {
+                  playerGuesses.add(false); // Schwarz
+                  Navigator.of(context).pop();
+                  askPlayerGuess();
+                },
+                child: Text('Schwarz'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      revealCardAndShowResults();
+    }
+  }
+
+  void revealCardAndShowResults() {
+    // Bestimme die Farbe der ausgewählten Karte
+    bool isCardRed = selectedCard!.suit == 'hearts' || selectedCard!.suit == 'diamonds';
+    List<bool> results = playerGuesses.map((guess) => guess == isCardRed).toList();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Ergebnisse'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: results.asMap().entries.map((entry) {
+                int idx = entry.key;
+                bool result = entry.value;
+                return Text('Spieler ${idx + 1}: ${result ? "Richtig" : "Falsch"}');
+              }).toList(),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Spiel'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Die Karte ist verdeckt. Rate die Farbe!'),
+            SizedBox(height: 30), // Erhöhter Platz zwischen den Buttons
+          ],
+        ),
+      ),
     );
   }
 }
